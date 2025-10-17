de5b978683bbca618cc55f44872f0ee58ab30697
SantiagoUribe269 <santiago.uribe@utec.edu.pe>
2025-10-16 22:37:02 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+def great(name: str) -> str:
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(great("World"))
---
+++ test/test_hello.py
@@ -0,0 +1,4 @@
+from hello import great
+
+def test_great():
+    assert great("UTEC!") == "UTEC!"
